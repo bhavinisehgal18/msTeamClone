02987f2485bf7582cd4ae48e4bc40c0e1b463598
bhavinisehgal18 <62087213+bhavinisehgal18@users.noreply.github.com>
2021-07-12 21:17:15 +0530

    Update events.js

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -20,35 +20,27 @@ window.addEventListener( 'load', () => {
             chatElem.classList.add( 'chat-opened' );
         }
 
-        //remove the 'New' badge on chat icon (if any) once chat is opened.
+        //remove the 'New' on chat icon once chat is opened.
         setTimeout( () => {
             if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
                 helpers.toggleChatNotificationBadge();
             }
         }, 300 );
     } );
-
-
-    //When the video frame is clicked. This will enable picture-in-picture
     document.getElementById( 'local' ).addEventListener( 'click', () => {
         if ( !document.pictureInPictureElement ) {
             document.getElementById( 'local' ).requestPictureInPicture()
                 .catch( error => {
-                    // Video failed to enter Picture-in-Picture mode.
                     console.error( error );
                 } );
         }
-
         else {
             document.exitPictureInPicture()
                 .catch( error => {
-                    // Video failed to leave Picture-in-Picture mode.
                     console.error( error );
                 } );
         }
     } );
-
-
     //When the 'Create room" is button is clicked
     document.getElementById( 'create-room' ).addEventListener( 'click', ( e ) => {
         e.preventDefault();
@@ -57,53 +49,32 @@ window.addEventListener( 'load', () => {
         let yourName = document.querySelector( '#your-name' ).value;
 
         if ( roomName && yourName ) {
-            //remove error message, if any
             document.querySelector( '#err-msg' ).innerHTML = "";
-
-            //save the user's name in sessionStorage
             sessionStorage.setItem( 'username', yourName );
 
-            //create room link
             let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
-
-            //show message with link to room
-            document.querySelector( '#room-created' ).innerHTML = `Room successfully created. Click <a href='${ roomLink }'>here</a> to enter room. 
-                Share the room link with your partners.`;
-
-            //empty the values
+            document.querySelector( '#room-created' ).innerHTML = `<a href='${ roomLink }'>Join</a>.`;
             document.querySelector( '#room-name' ).value = '';
             document.querySelector( '#your-name' ).value = '';
         }
-
         else {
             document.querySelector( '#err-msg' ).innerHTML = "All fields are required";
         }
     } );
-
-
-    //When the 'Enter room' button is clicked.
     document.getElementById( 'enter-room' ).addEventListener( 'click', ( e ) => {
         e.preventDefault();
 
         let name = document.querySelector( '#username' ).value;
 
         if ( name ) {
-            //remove error message, if any
             document.querySelector( '#err-msg-username' ).innerHTML = "";
-
-            //save the user's name in sessionStorage
             sessionStorage.setItem( 'username', name );
-
-            //reload room
             location.reload();
         }
-
         else {
             document.querySelector( '#err-msg-username' ).innerHTML = "Please input your name";
         }
     } );
-
-
     document.addEventListener( 'click', ( e ) => {
         if ( e.target && e.target.classList.contains( 'expand-remote-video' ) ) {
             helpers.maximiseStream( e );
@@ -113,8 +84,6 @@ window.addEventListener( 'load', () => {
             helpers.singleStreamToggleMute( e );
         }
     } );
-
-
     document.getElementById( 'closeModal' ).addEventListener( 'click', () => {
         helpers.toggleModal( 'recording-options-modal', false );
     } );
